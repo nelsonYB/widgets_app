ff7f7e203eeb88026680f1babb44f409f5ec2d5b
Nelson <nel.ubeda@gmail.com>
2023-08-29 19:08:43 +0200

    animated container terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -48,4 +48,11 @@ const appMenuItems = <MenuItem> [
     icon: Icons.info_outline,
   ),
 
+  MenuItem(
+    title: 'Animated container', 
+    subTitle: 'Stateful widget animado', 
+    link: '/animated', 
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
+
 ];
