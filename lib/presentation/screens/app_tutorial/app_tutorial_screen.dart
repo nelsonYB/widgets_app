8d9d56479d91fe047f1bf6c4640b538b3543d628
Nelson <nel.ubeda@gmail.com>
2023-09-01 16:59:25 +0200

    tutorial finalizado

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,13 +1,152 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo('Busca la comida', 'Exercitation voluptate cillum eu aute dolor irure aliquip.', 'assets/images/1.png'),
+  SlideInfo('Entrega rápida', 'Exercitation voluptate cillum eu aute dolor irure aliquip.', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'Exercitation voluptate cillum eu aute dolor irure aliquip.', 'assets/images/3.png'),
+];
+
+
+
+class AppTutorialScreen extends StatefulWidget {
 
   static const name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+
+  final PageController pageViewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // esto es básicamente para manejar en que slide se encuentra y así manejar los botones
+    pageViewController.addListener(() {
+
+      final page = pageViewController.page ?? 0;
+      // esta condición valora en que slide estamos para mostrar el botón o no
+      if ( !endReached && page >= (slides.length - 1.5) ) {
+        setState(() {
+          endReached = true;
+        });
+      }
+      
+
+    });
+
+  }
+
+  // Es muy buena práctica llamar el dispose que es otra parte del ciclo de
+  // vide de los stateFullWidgets
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller:pageViewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides.map(
+              (slideData) => _Slide(
+                title: slideData.title, 
+                caption: slideData.caption, 
+                imageUrl: slideData.imageUrl
+              )
+            ).toList(),
+          ),
+
+          Positioned(
+            right: 20,
+            top: 50, // esto va de arriba hacia abajo
+            child: TextButton(
+              child: const Text('Salir'),
+              onPressed: () => context.pop(),
+            )
+          ),
+
+          endReached ? // esto es una condicional para mostrar el widget de abajo
+            Positioned(
+              right: 30, // esto va de derecha a izquierda
+              bottom: 30, // esto va de abajo hacia arriba
+              child: FadeInRight(
+                from: 15,
+                delay: const Duration(seconds: 1),
+                child: FilledButton(
+                  child: const Text('Comenzar'),
+                  onPressed: () => context.pop(),
+                ),
+              )
+            ): const SizedBox(),
+        ],
+      ),
+
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title, 
+    required this.caption, 
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage( imageUrl )),
+            const SizedBox(height: 20),
+            Text( title, style: titleStyle ),
+            const SizedBox(height: 10),
+            Text( caption, style: captionStyle ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
