3e3582f349ba7f5971e58cdb72c27a059c993882
Nelson <nel.ubeda@gmail.com>
2023-09-01 19:25:52 +0200

    infiniteScroll finalizado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -69,4 +69,11 @@ const appMenuItems = <MenuItem> [
     icon: Icons.accessible_rounded,
   ),
 
+  MenuItem(
+    title: 'InfiniteScroll y Pull', 
+    subTitle: 'Listas infinitas y pull to refresh', 
+    link: '/infinite', 
+    icon: Icons.list_alt_rounded,
+  ),
+
 ];
